fd5f0a3fc5442e88259a8273a56fba31d0f324c6
Robert Dahlborg <robert.dahlborg@kindredgroup.com>
2023-05-30 12:07:36 +0200

    feat: (authentication): Add JWT token validation endpoint
    This commit adds a new endpoint '/validate_token' for validating JWT tokens. The endpoint receives a token in the request body and verifies it using the app's secret key. If the token is valid, the payload data is accessed and additional validation logic can be added. The endpoint returns a JSON response with a 'valid' flag and the user_id if the token is valid, or an error message if it's invalid.

--- app.py
+++ app.py
@@ -1,16 +1,30 @@
-from flask import Flask, jsonify
-
- 
+from flask import Flask, request, jsonify
+import jwt
 
 app = Flask(__name__)
 
- 
+# Secret key for JWT token verification
+app.config['SECRET_KEY'] = 'your-secret-key'
 
-@app.route('/', methods=['GET'])
-def home():
-    return jsonify(message="Hello, World!")
+@app.route('/validate_token', methods=['POST'])
+def validate_token():
+    token = request.json['token']
 
- 
+    try:
+        # Verify and decode the JWT token
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+        
+        # Access the payload data
+        user_id = decoded_token['user_id']
+        
+        # Add your additional validation logic here
+        # ...
+
+        return jsonify(valid=True, user_id=user_id)
+    except jwt.ExpiredSignatureError:
+        return jsonify(valid=False, error='Token has expired')
+    except jwt.InvalidTokenError:
+        return jsonify(valid=False, error='Invalid token')
 
 if __name__ == '__main__':
     app.run(debug=True)
